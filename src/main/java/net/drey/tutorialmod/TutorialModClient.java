85f5d5e56421c0215f8e8a50d58aa3313f846997
Adeqtz <andreasgman@hotmail.com>
2025-08-19 20:00:05 +1000

    Initial commit

---
+++ src/main/java/net/drey/tutorialmod/TutorialModClient.java
@@ -0,0 +1,10 @@
+package net.drey.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
