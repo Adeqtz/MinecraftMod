9ef0f9598e6bd526cb5856a0d922a1403e5a52c5
Adeqtz <andreasgman@hotmail.com>
2025-08-23 21:28:40 +1000

    Moved all data for items/blocks to streamlined data generation method

---
+++ src/main/java/net/drey/tutorialmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,26 @@
+package net.drey.tutorialmod.datagen;
+
+import net.drey.tutorialmod.item.ModItems;
+import net.drey.tutorialmod.util.ModTags;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.item.Items;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+            getOrCreateTagBuilder(ModTags.Items.TRANSFORMABLE_ITEMS)
+                    .add(ModItems.PINK_GARNET)
+                    .add(ModItems.RAW_PINK_GARNET)
+                    .add(Items.COAL)
+                    .add(Items.APPLE)
+                    .add(Items.STICK);
+    }
+}
---
+++ src/main/java/net/drey/tutorialmod/datagen/ModModelProvider.java
@@ -0,0 +1,36 @@
+package net.drey.tutorialmod.datagen;
+
+import net.drey.tutorialmod.block.ModBlocks;
+import net.drey.tutorialmod.item.ModItems;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricModelProvider;
+import net.minecraft.data.client.BlockStateModelGenerator;
+import net.minecraft.data.client.ItemModelGenerator;
+import net.minecraft.data.client.Models;
+
+public class ModModelProvider extends FabricModelProvider {
+    public ModModelProvider(FabricDataOutput output) {
+        super(output);
+    }
+
+    @Override
+    public void generateBlockStateModels(BlockStateModelGenerator blockStateModelGenerator) {
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PINK_GARNET_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PINK_GARNET_DEEPSLATE_ORE);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.RAW_PINK_GARNET_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.MAGIC_BLOCK);
+        blockStateModelGenerator.registerSimpleCubeAll(ModBlocks.PINK_GARNET_ORE);
+    }
+
+    @Override
+    public void generateItemModels(ItemModelGenerator itemModelGenerator) {
+        itemModelGenerator.register(ModItems.PINK_GARNET, Models.GENERATED);
+        itemModelGenerator.register(ModItems.RAW_PINK_GARNET, Models.GENERATED);
+        itemModelGenerator.register(ModItems.CAULIFLOWER, Models.GENERATED);
+        itemModelGenerator.register(ModItems.CHISEL, Models.GENERATED);
+        itemModelGenerator.register(ModItems.SCYTHE, Models.GENERATED);
+
+
+
+    }
+}
